4b4138396ac7ee308093cbf0bc4af5133174a0b5
kunal <kunal.dhote7799@gmail.com>
2018-10-11 12:52:42 +0530

    Adding MiniStatement Module

--- Guru99BankHybridFrameWork/src/test/java/com/guru99bank/pageObject/MiniStatementPage.java
+++ Guru99BankHybridFrameWork/src/test/java/com/guru99bank/pageObject/MiniStatementPage.java
@@ -1,5 +1,52 @@
 package com.guru99bank.pageObject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MiniStatementPage {
+	
+	public static WebDriver driver;
+	
+	@FindBy(xpath="//*[text()='Mini Statement']")
+	WebElement btn_mini;
+	
+	
+	@FindBy(name="accountno")
+	WebElement txt_accountno;
+	
+	
+	@FindBy(name="AccSubmit")
+	WebElement btn_submmit;
+	
+	public MiniStatementPage(WebDriver driver)
+	{
+		
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void click_ministatement()
+	{
+		
+		btn_mini.click();
+		
+	}
+	
+	
+	public void enterAccountno(String aid)
+	{
+		txt_accountno.sendKeys(aid);
+		
+		
+	}
+	
+	public void click_submmit()
+	{
+		btn_submmit.click();
+		
+	}
 
 }
--- Guru99BankHybridFrameWork/src/test/java/com/guru99bank/testCases/MiniStatement.java
+++ Guru99BankHybridFrameWork/src/test/java/com/guru99bank/testCases/MiniStatement.java
@@ -1,5 +1,31 @@
 package com.guru99bank.testCases;
 
-public class MiniStatement {
+import org.testng.annotations.Test;
+
+import com.guru99bank.pageObject.LoginPage;
+import com.guru99bank.pageObject.MiniStatementPage;
+
+public class MiniStatement extends BaseClass {
+	
+	
+	LoginPage login;
+	MiniStatementPage ministatement;
+	
+	@Test(priority=15 ,description="Verifying MiniStatement Module")
+	public void verifyMinistatement() throws InterruptedException
+	{
+		login = new LoginPage(driver);
+		login.setUsaername(userName);
+		login.setPassword(passWord);
+		login.setLogin();
+		
+		Thread.sleep(2000);
+		
+		ministatement = new MiniStatementPage(driver);
+		
+		ministatement.click_ministatement();
+		ministatement.enterAccountno("49730");
+		ministatement.click_submmit();
+	}
 
 }
